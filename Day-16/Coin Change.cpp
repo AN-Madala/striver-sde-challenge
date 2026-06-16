ff4b9f698a0e3bd25bb4baaae3cfba26bc8aae4b
Aditya Narayan Madala <165414605+AN-Madala@users.noreply.github.com>
2026-06-16 23:04:47 +0530

    Added solutions of Coin Change

---
+++ Day-16/Coin Change.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+        int n = coins.size();
+
+        vector<vector<int>> dp(
+            n,
+            vector<int>(amount + 1, 0)
+        );
+
+        for(int target = 0;
+            target <= amount;
+            target++) {
+
+            if(target % coins[0] == 0)
+                dp[0][target]
+                    = target / coins[0];
+            else
+                dp[0][target]
+                    = 1e9;
+        }
+
+        for(int ind = 1;
+            ind < n;
+            ind++) {
+
+            for(int target = 0;
+                target <= amount;
+                target++) {
+
+                int notTake =
+                    dp[ind - 1][target];
+
+                int take = 1e9;
+
+                if(coins[ind] <= target)
+                    take = 1 +
+                           dp[ind]
+                             [target - coins[ind]];
+
+                dp[ind][target]
+                    = min(take, notTake);
+            }
+        }
+
+        int ans = dp[n - 1][amount];
+
+        return ans >= 1e9 ? -1 : ans;
+    }
+};
