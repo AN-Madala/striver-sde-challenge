8a0fc8fa82c86a33d6dba2cc6b97d5828728b31b
Aditya Narayan Madala <165414605+AN-Madala@users.noreply.github.com>
2026-06-17 22:42:38 +0530

    Added solution of Combination Sum II

---
+++ Day-17/Combination Sum II.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+
+    void solve(int ind,
+               int target,
+               vector<int>& candidates,
+               vector<int>& ds,
+               vector<vector<int>>& ans) {
+
+        if(target == 0) {
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i = ind; i < candidates.size(); i++) {
+
+            // Skip duplicates
+            if(i > ind &&
+               candidates[i] == candidates[i - 1])
+                continue;
+
+            // No need to explore further
+            if(candidates[i] > target)
+                break;
+
+            ds.push_back(candidates[i]);
+
+            solve(i + 1,
+                  target - candidates[i],
+                  candidates,
+                  ds,
+                  ans);
+
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(
+        vector<int>& candidates,
+        int target) {
+
+        sort(candidates.begin(),
+             candidates.end());
+
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        solve(0,
+              target,
+              candidates,
+              ds,
+              ans);
+
+        return ans;
+    }
+};
