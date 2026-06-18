80dc5aa45e82cd7dcd29da105987bf696d684864
Aditya Narayan Madala <165414605+AN-Madala@users.noreply.github.com>
2026-06-18 22:55:04 +0530

    Added solution of Permutations

---
+++ Day-18/Permutations.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    void solve(vector<int>& nums,
+               vector<int>& ds,
+               vector<int>& freq,
+               vector<vector<int>>& ans) {
+
+        if(ds.size() == nums.size()) {
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            if(!freq[i]) {
+
+                freq[i] = 1;
+                ds.push_back(nums[i]);
+
+                solve(nums,
+                      ds,
+                      freq,
+                      ans);
+
+                ds.pop_back();
+                freq[i] = 0;
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        vector<int> freq(nums.size(), 0);
+
+        solve(nums,
+              ds,
+              freq,
+              ans);
+
+        return ans;
+    }
+};
